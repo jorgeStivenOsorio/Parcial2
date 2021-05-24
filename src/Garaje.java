a9b2c0dd10d2cf7e5d180775345a0f185296b370
stiven <paisa2207@gmail.com>
2021-05-23 22:04:40 -0500

    simplifique algunas opciones

--- src/Garaje.java
+++ src/Garaje.java
@@ -1,9 +1,10 @@
 import model.Color;
 import model.Vehiculo;
+
 import java.util.*;
 
 public class Garaje {
-    Set<Vehiculo>garaje = new HashSet<>();
+    Set<Vehiculo> garaje = new HashSet<>();
     Datos datos = new Datos();
 
     public void ingresarVehiculoAlGaraje(int opcion) {
@@ -12,29 +13,46 @@ public class Garaje {
         System.out.println("¡Vehiculo agregado exitosamente!");
     }
 
-    public void agregarAvion() { ingresarVehiculoAlGaraje(1); }
+    public void agregarAvion() {
+        ingresarVehiculoAlGaraje(1);
+    }
 
-    public void agregarYate() { ingresarVehiculoAlGaraje(2); }
+    public void agregarYate() {
+        ingresarVehiculoAlGaraje(2);
+    }
 
-    public void agregarCarro() { ingresarVehiculoAlGaraje(3); }
+    public void agregarCarro() {
+        ingresarVehiculoAlGaraje(3);
+    }
 
     public void mostrarGaraje() {
-        if (garaje.isEmpty()){
+        if (garaje.isEmpty()) {
             System.out.println("¡El garaje esta vacio!");
-        }else {
+        } else {
             garaje.forEach(System.out::println);
         }
     }
 
-    public void vehiculoMasRapido(){garaje.stream().max(Comparator.comparing(Vehiculo::getVelocidadMaxima)).stream().forEach(System.out::println);}
-    public void vehiculoMasLento(){garaje.stream().min(Comparator.comparing(Vehiculo::getVelocidadMaxima)).stream().forEach(System.out::println);}
-    public void ordenarVehiculos(){garaje.stream().sorted((p1, p2) -> p2.getVelocidadMaxima() - p1.getVelocidadMaxima()).forEach(System.out::println);}
-    public void vehiculosNegrosAzules(){ garaje.stream().filter(c -> c.getColor() == Color.NEGRO || c.getColor() == Color.AZUL).forEach(System.out::println); }
-    public void referenciaConcatenada(){
-        String list = garaje.stream().filter(c -> c.getColor() == Color.BLANCO || c.getColor() == Color.VERDE).map(p -> p.getReferencia()).reduce("",(acc, num) -> acc + num);
-        System.out.println(list);
+    public void vehiculoMasRapido() {
+        garaje.stream().max(Comparator.comparing(Vehiculo::getVelocidadMaxima)).ifPresent(System.out::println);
     }
 
+    public void vehiculoMasLento() {
+        garaje.stream().min(Comparator.comparing(Vehiculo::getVelocidadMaxima)).ifPresent(System.out::println);
+    }
+
+    public void ordenarVehiculos() {
+        garaje.stream().sorted((p1, p2) -> p2.getVelocidadMaxima() - p1.getVelocidadMaxima()).forEach(System.out::println);
+    }
+
+    public void vehiculosNegrosAzules() {
+        garaje.stream().filter(c -> c.getColor() == Color.NEGRO || c.getColor() == Color.AZUL).forEach(System.out::println);
+    }
+
+    public void referenciaConcatenada() {
+        String list = garaje.stream().filter(c -> c.getColor() == Color.BLANCO || c.getColor() == Color.VERDE).map(Vehiculo::getReferencia).reduce("", (acc, num) -> acc + num);
+        System.out.println(list);
+    }
 
 
 }
